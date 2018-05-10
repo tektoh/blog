7d20337a80d7caca58df90a55a85303bb30b59e2
Shinichi Segawa <tektoh@gmail.com>
2018-05-11 00:07:02 +0900

    修正

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,4 +1,8 @@
 if Rails.env.staging? || Rails.env.production?
   Resque.redis = Redis.new(Settings.resque.redis.to_h)
   Resque.after_fork = proc { ActiveRecord::Base.establish_connection }
+
+  logger = ActiveSupport::Logger.new(Rails.root.join('log', "#{Rails.env}_resque.log"), 5, 10 * 1024 * 1024)
+  logger.formatter = config.log_formatter
+  Resque.logger = ActiveSupport::TaggedLogging.new(logger)
 end
