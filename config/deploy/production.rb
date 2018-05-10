1c2ff3c76e2b82a08ac9f48580c93fb359bae607
Shinichi Segawa <tektoh@gmail.com>
2018-05-10 22:36:14 +0900

    修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,15 +1,14 @@
 set :stage, :production
 set :rails_env, 'production'
 set :migration_role, 'db'
-set :deploy_to, '/var/www/start-dash'
 set :linked_dirs, %w[log tmp/backup tmp/pids tmp/cache tmp/sockets vendor/bundle node_modules public/packs]
-set :workers, '*' => 2
+set :workers, '*' => 1
 
 set :whenever_environment, :production
 set :whenever_roles, :batch
 
-role :app, %w[devuser@app1.start-dash.startup-technology.com]
-role :web, %w[devuser@app1.start-dash.startup-technology.com]
-role :db, %w[devuser@app1.start-dash.startup-technology.com]
-role :batch, %w[devuser@app1.start-dash.startup-technology.com]
-role :resque_worker, %w[devuser@app1.start-dash.startup-technology.com]
+role :app, %w[blog@web1.production.blog-dev.com]
+role :web, %w[blog@web1.production.blog-dev.com]
+role :db, %w[blog@web1.production.blog-dev.com]
+role :batch, %w[blog@web1.production.blog-dev.com]
+role :resque_worker, %w[blog@web1.production.blog-dev.com]
--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,4 +1,4 @@
 if Rails.env.staging? || Rails.env.production?
-  Resque.redis = Redis.new(Settings.resque.redis)
+  Resque.redis = Redis.new(Settings.resque.redis.to_h)
   Resque.after_fork = proc { ActiveRecord::Base.establish_connection }
 end
