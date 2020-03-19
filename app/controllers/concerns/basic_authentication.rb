2e22db80b092109a28c8ce28196fa23e41a009b9
Shinichi Segawa <tektoh@gmail.com>
2020-03-19 13:53:14 +0900

    fix rubocop

--- app/controllers/concerns/basic_authentication.rb
+++ app/controllers/concerns/basic_authentication.rb
@@ -4,18 +4,17 @@ module BasicAuthentication
   extend ActiveSupport::Concern
 
   private
+    def authenticate_http_basic
+      return if authenticate_http_basic?
 
-  def authenticate_http_basic
-    return if authenticate_http_basic?
-
-    authenticate_or_request_with_http_basic do |user, password|
-      user == Settings.basic_authentication.user &&
-        password == Settings.basic_authentication.password
+      authenticate_or_request_with_http_basic do |user, password|
+        user == Settings.basic_authentication.user &&
+          password == Settings.basic_authentication.password
+      end
     end
-  end
 
-  def authenticate_http_basic?
-    Settings.basic_authentication&.user.blank? ||
-      request.user_agent == Settings.basic_authentication.allow_user_agent
-  end
+    def authenticate_http_basic?
+      Settings.basic_authentication&.user.blank? ||
+        request.user_agent == Settings.basic_authentication.allow_user_agent
+    end
 end
--- app/controllers/concerns/host_redirect.rb
+++ app/controllers/concerns/host_redirect.rb
@@ -4,12 +4,11 @@ module HostRedirect
   extend ActiveSupport::Concern
 
   private
+    def redirect_to_site_host
+      return if Settings.host.blank?
 
-  def redirect_to_site_host
-    return if Settings.host.blank?
-
-    if request.host != Settings.host
-      redirect_to host: Settings.host, status: 301
+      if request.host != Settings.host
+        redirect_to host: Settings.host, status: 301
+      end
     end
-  end
 end
