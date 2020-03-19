a0b26637cd07f7da6505820e2446df5291a340a6
Shinichi Segawa <tektoh@gmail.com>
2020-03-19 15:13:10 +0900

    修正

--- config/puma.rb
+++ config/puma.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+workers ENV.fetch("WEB_CONCURRENCY") { 1 }
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
 threads threads_count, threads_count
 
